2888c87e19b3362f84eea769a79309783744ef41
SUMAYYA <SUMAYYA@LAPTOP-JTB05SP0>
2024-11-19 09:46:48 +0530

    Final commit

---
+++ FirstMavenProject/src/main/java/firstmavenpack/Add.java
@@ -0,0 +1,13 @@
+package firstmavenpack;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=3;
+int b=8;
+int c=a+b;
+System.out.println(c);
+	}
+
+}
---
+++ FirstMavenProject/src/main/java/firstmavenpack/Factorial.java
@@ -0,0 +1,17 @@
+package firstmavenpack;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i;
+int count=1;
+for(i=5;i>=1;i--)
+{
+	count=count*i;
+			
+}
+System.out.println("Factorial of given number is"+"  "+count);
+	}
+
+}
